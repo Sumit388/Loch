fb63da0eef174b6abc711481c7b0479a6fd08a78
Sumit Mishra <sumit.mishra@uniacco.com>
2023-12-08 22:49:43 +0530

    Feat: Made notifications card auto scroll

--- src/components/Homepage/GetNotified.tsx
+++ src/components/Homepage/GetNotified.tsx
@@ -1,32 +1,32 @@
+/* //* Packages Imports */
+import Marquee from "react-fast-marquee";
+
 //* Component Imports */
 import DetailsCard from "@Components/Cards/DetailsCard";
 import NotificationCard from "@Components/Cards/NotificationCard";
 import AmountLimitCard from "@Components/Cards/AmountLimitCard";
 import DaysLimitCard from "@Components/Cards/DaysLimitCard";
 
-/* //* Assets Imports */
-import Bell from "@Assets/Bell.svg";
+/* //* Data Imports */
+import { GET_NOTIFIED } from "@Data/CardDescriptions";
 
 /* //* Styles Imports */
 import Styles from "@Styles/Homepage/GetNotified.module.scss";
 
 function GetNotified() {
-  const data = {
-    icon: Bell,
-    heading: "Get notified when a highly correlated whale makes a move",
-    description:
-      "Find out when a certain whale moves more than any preset amount on-chain or when a dormant whale you care about becomes active.",
-  };
+  
   return (
     <div className={Styles.mainContainer}>
       <div className={Styles.detailsContainer}>
-        <DetailsCard data={data} />
+        <DetailsCard data={GET_NOTIFIED} />
       </div>
-      <div className={Styles.utilitiesContainer}>
-        <NotificationCard />
-        <AmountLimitCard />
-        <DaysLimitCard />
-      </div>
+      <Marquee speed={30} pauseOnHover className={Styles.marqueeConatiner}>
+        <div className={Styles.utilitiesContainer}>
+          <NotificationCard />
+          <AmountLimitCard />
+          <DaysLimitCard />
+        </div>
+      </Marquee>
     </div>
   );
 }
--- src/data/CardDescriptions.ts
+++ src/data/CardDescriptions.ts
@@ -1,4 +1,5 @@
 import Eye from "@Assets/Eye.svg";
+import Bell from "@Assets/Bell.svg";
 
 export const WATCH_WHALES = {
   icon: Eye,
@@ -6,3 +7,10 @@ export const WATCH_WHALES = {
   description:
     "All whales are not equal. Know exactly what the whales impacting YOUR portfolio are doing.",
 };
+
+export const GET_NOTIFIED = {
+    icon: Bell,
+    heading: "Get notified when a highly correlated whale makes a move",
+    description:
+      "Find out when a certain whale moves more than any preset amount on-chain or when a dormant whale you care about becomes active.",
+  };
